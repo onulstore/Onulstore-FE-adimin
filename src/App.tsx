5f340c35c275abdd07868dd9f1481bfd5848db2a
k-m-jin <mel917@naver.com>
2022-09-27 23:04:31 +0900

    conf: 기본 설정

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { Routes, Route } from 'react-router-dom';
 import GlobalStyles from './style/global';
 import theme from '../src/style/theme';
 import { ThemeProvider } from 'styled-components';
+//PAGES
 import Home from './pages/Home';
 
 const App = () => {
