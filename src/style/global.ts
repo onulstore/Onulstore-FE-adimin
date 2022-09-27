7a2ca933906ff402b623b7e8cb63979067d76956
k-m-jin <mel917@naver.com>
2022-09-27 23:18:59 +0900

    conf: style 기본설정 수정

--- src/style/global.ts
+++ src/style/global.ts
@@ -1,39 +1,8 @@
 import { createGlobalStyle } from 'styled-components';
 import reset from 'styled-reset';
-/**fonts */
-import MontserratBold from 'assets/fonts/Montserrat-Bold.ttf';
-import MontserratRegular from 'assets/fonts/Montserrat-Regular.ttf';
-import MontserratSemiBold from 'assets/fonts/Montserrat-SemiBold.ttf';
-import PretendardBlack from 'assets/fonts/Pretendard-Black.otf';
-import PretendardBold from 'assets/fonts/Pretendard-Bold.otf';
-import PretendardRegular from 'assets/fonts/Pretendard-Regular.otf';
 
 export default createGlobalStyle`
-  @font-face {
-    font-family: "MontserratBold";
-    src: url(${MontserratBold}) format('truetype');
-  }
-  @font-face {
-    font-family: "MontserratRegular";
-    src: url(${MontserratRegular}) format('truetype');
-  }
-  @font-face {
-    font-family: "MontserratSemiBold";
-    src: url(${MontserratSemiBold}) format('truetype');
-  }
-  @font-face {
-    font-family: "PretendardBlack";
-    src: url(${PretendardBlack}) format('opentype');
-  }
-  @font-face {
-    font-family: "PretendardBold";
-    src: url(${PretendardBold}) format('opentype');
-  }
-  @font-face {
-    font-family: "PretendardRegular";
-    src: url(${PretendardRegular}) format('opentype');
-  }
-  
+ 
 ${reset}
 
 * {
@@ -44,8 +13,6 @@ html {
   /* rem 값의 기준을 잡기 위함 16px -> 10px */
 }
 body {
-  font-family: 'MontserratSemiBold' ,serif !important;
-  /* max-width: 375px; */
   margin: 0;
 }
 input, textarea { 
