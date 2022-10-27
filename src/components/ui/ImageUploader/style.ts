79f8343887881c58f11b52da7c07590673a5d4a7
hoona1011 <hoona1011@gmail.com>
2022-10-28 02:17:45 +0900

    #11 refactor: base64 => multipart/form 이미지 업로드 방식 변경

--- src/components/ui/ImageUploader/style.ts
+++ src/components/ui/ImageUploader/style.ts
@@ -1,23 +1,33 @@
 import styled from 'styled-components';
 
 export const Container = styled.div`
-  img {
-    width: 100px;
+  .image {
+    position: relative;
+
+    button {
+      position: absolute;
+      right: ${({ isBig }) => (isBig ? '1.6rem' : '0.7rem')};
+      top: ${({ isBig }) => (isBig ? '1.6rem' : '0.7rem')};
+    }
   }
 `;
 
+export const Image = styled.img`
+  width: ${({ isBig }) => (isBig ? '36rem' : '17rem')};
+  height: ${({ isBig }) => (isBig ? '36rem' : '17rem')};
+`;
 export const Button = styled.button`
-  width: 23.852rem;
-  height: 11.9rem;
-  padding: 3.5rem 5.7rem;
-  border-radius: 1rem;
+  /* width: 23.852rem; */
+  /* height: 11.9rem; */
+  /* padding: 3.5rem 5.7rem; */
+  /* border-radius: 1rem; */
   cursor: pointer;
 
   div {
-    margin-top: 0.8rem;
+    /* margin-top: 0.8rem;
     font-size: 1.2rem;
     font-weight: 500;
-    line-height: 2rem;
+    line-height: 2rem; */
   }
 
   input {
